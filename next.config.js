9be3f059eb6426c6863de2ca7ce0fa2adf4298b0
Chung Arguello <arguello.chung@gmail.com>
2022-06-29 11:48:16 -0400

    added a proxy for magic eden api in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.export = {
-  images: {
-    loader: "imgix",
-    domains: ["gateway.pinata.cloud"],
-  },
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: "/dashboard",
+        destination: "https://api-mainnet.magiceden.dev/v2",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
 };
