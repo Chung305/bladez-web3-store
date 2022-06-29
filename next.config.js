8c84f1357cf3b91dfffadaeba14b6b91ca8e85bd
Chung Arguello <arguello.chung@gmail.com>
2022-06-29 12:13:23 -0400

    corss

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-module.exports = () => {
-  const rewrites = () => {
-    return [
-      {
-        source: "/dashboard",
-        destination: "https://api-mainnet.magiceden.dev/v2",
-      },
-    ];
-  };
-  return {
-    rewrites,
-  };
-};
+// module.exports = () => {
+//   const rewrites = () => {
+//     return [
+//       {
+//         source: "/dashboard",
+//         destination: "https://api-mainnet.magiceden.dev/v2",
+//       },
+//     ];
+//   };
+//   return {
+//     rewrites,
+//   };
+// };
